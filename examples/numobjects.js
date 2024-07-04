5b62b5f8293709f3980b72c7772b4254fee60ddb
Helio Tejeda <htejeda@uci.edu>
2024-07-03 20:23:53 -0700

    Added numobjects files and added the guessing game link to the index file

--- examples/numobjects.js
+++ examples/numobjects.js
@@ -23,7 +23,7 @@ import {
 export const studyId = 'numobjects'; 
 // Configuration setting for the session
 export const sessionConfig = {
-    minPlayersNeeded: 1, // Minimum number of players needed; if set to 1, there is no waiting room (unless a countdown has been setup)
+    minPlayersNeeded: 2, // Minimum number of players needed; if set to 1, there is no waiting room (unless a countdown has been setup)
     maxPlayersNeeded: 5, // Maximum number of players allowed in a session
     maxParallelSessions: 0, // Maximum number of sessions in parallel (if zero, there are no limit)
     allowReplacements: false, // Allow replacing any players who leave an ongoing session?
@@ -36,6 +36,8 @@ export const verbosity = 2;
 
 // Allow URL parameters to update these default parameters
 updateConfigFromUrl( sessionConfig );
+
+//console.log("THIS IS MY OWN TEST: ", sessionInfo.numPlayers)
 
 // -------------------------------------
 //       Globals
@@ -77,3 +79,214 @@ joinButton.addEventListener('click', function () {
 leaveButton.addEventListener('click', function () {
     leaveSession(); // call the library function to leave a session. This then triggers the local function endSession
 });
+
+
+// --------------------------------------------------------------------------------------
+//   Handle Events triggered by MPLIB
+//   These callback functions are required, but the contents can be empty and left inconsequential  
+//   (note: all timestamps are server-side expressed in milliseconds since the Unix Epoch)
+// --------------------------------------------------------------------------------------
+// Function to receive state changes from Firebase
+export function receiveStateChange(nodeName, newState, typeChange ) {
+    /*if (nodeName === 'state') {
+        gameState = newState;
+        updateUI();
+
+        if (gameState.winner !== emptyPlace) {
+            setTimeout(function() {
+                // Propose a new game
+                newGame();
+            }, delayStartNewGame );
+        }
+    }*/
+}
+
+
+export function evaluateUpdate( path, state, action, actionArgs ) {
+    /*let isAllowed = false;
+    let newState = null;
+
+    if ((action === 'initialize') && ((state === null) || state.winner !== emptyPlace)) {
+        isAllowed = true;
+        newState = actionArgs;
+    }
+
+    if ((action === 'placeToken') && (state.winner === emptyPlace )) {
+        // Is it this player's turn?
+        if (((thisSession.arrivalIndex===1) && (state.currentPlayer === state.firstArrival)) ||   
+            ((thisSession.arrivalIndex===2) && (state.currentPlayer !== state.firstArrival))) {
+
+            // Can a token be placed here?
+            let placeIndex = actionArgs;
+            if ((state.board[ placeIndex ] !== 'X') && (state.board[ placeIndex ] !== 'O')) {
+                isAllowed = true;
+                newState = state;
+                newState.board[placeIndex] = newState.currentPlayer;
+                
+                // Check if all places are filled
+                const isBoardFilledWithXorO = newState.board.every(cell => cell === 'X' || cell === 'O');
+
+                // Is this a winning move?
+                if (checkWinner( newState.board )) {
+                    newState.winner = newState.currentPlayer;
+                } else if (isBoardFilledWithXorO) {
+                    // A draw                   
+                    newState.winner = 'XO';
+                } else {
+                    // Give the turn to the next player
+                    newState.currentPlayer = newState.currentPlayer === 'X' ? 'O' : 'X';
+                }
+                
+            }        
+        }
+    }
+
+    let actionResult = { isAllowed, newState };
+    return actionResult;*/
+}
+
+
+// This callback function is triggered when a waiting room starts
+export function joinedWaitingRoom(sessionInfo) {
+    instructionsScreen.style.display = 'none';
+    waitingRoomScreen.style.display = 'block';
+
+    let numNeeded = sessionConfig.minPlayersNeeded - sessionInfo.numPlayers;
+    let numPlayers = sessionInfo.numPlayers;
+    let str2 = `Waiting for ${ numNeeded } additional ${ numPlayers > 1 ? 'players' : 'player' }...`;
+
+    messageWaitingRoom.innerText = str2;
+    thisSession = sessionInfo;
+}
+
+// This callback function is triggered when waiting room is still ongoing, but number of players waiting changes
+export function updateWaitingRoom(sessionInfo) {
+    instructionsScreen.style.display = 'none';
+    waitingRoomScreen.style.display = 'block';
+    let numNeeded = sessionConfig.minPlayersNeeded - sessionInfo.numPlayers;
+    let numPlayers = sessionInfo.numPlayers;
+    let str2;
+    if (sessionInfo.status == 'waitingRoomCountdown') {
+        str2 = `Game will start in ${ sessionInfo.countdown } seconds...`;
+    }  else {       
+        str2 = `Waiting for ${ numNeeded } additional ${ numPlayers > 1 ? 'players' : 'player' }...`;
+    } 
+    messageWaitingRoom.innerText = str2;
+    thisSession = sessionInfo;
+}
+
+// This callback function is triggered when the session starts (when enough players have gathered, or when only a single player is needed)
+export function startSession(sessionInfo) {
+    instructionsScreen.style.display = 'none';
+    waitingRoomScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
+    
+    let dateString = timeStr(sessionInfo.sessionStartedAt);
+    let str = `Started game with session id ${sessionInfo.sessionIndex} with ${sessionInfo.numPlayers} players at ${dateString}.`;
+    myconsolelog( str );
+
+    let str2 = `<p>The game has started...</p><p>Number of players: ${ sessionInfo.numPlayers}</p><p>Session ID: ${ sessionInfo.sessionId}$</p>`;
+    //messageGame.innerHTML = str2;
+
+    thisSession = sessionInfo;
+    newGame();
+}
+
+// This callback function is triggered when session is active, but number of players changes
+export function updateSession(sessionInfo) {    
+    let dateString = timeStr(sessionInfo.sessionStartedAt);
+    let str = `Started game with session id ${sessionInfo.sessionIndex} with ${sessionInfo.numPlayers} players at ${dateString}.`;
+    myconsolelog( str );
+
+    let str2 = `<p>The game has started...</p><p>Number of players: ${ sessionInfo.numPlayers}</p><p>Session ID: ${ sessionInfo.sessionId}$</p>`;
+    //messageGame.innerHTML = str2;
+    thisSession = sessionInfo;
+
+    if (sessionInfo.numPlayers == 1) {
+        instructionsScreen.style.display = 'none';
+        waitingRoomScreen.style.display = 'none';
+        gameScreen.style.display = 'none';
+        finishScreen.style.display = 'block';
+        messageFinish.innerHTML = `<p>The other player has left the session.</p>`;
+        leaveSession();
+    }
+}
+
+export function endSession( sessionInfo ) {
+    instructionsScreen.style.display = 'none';
+    waitingRoomScreen.style.display = 'none';
+    gameScreen.style.display = 'none';
+    finishScreen.style.display = 'block';
+
+    if (sessionInfo.sessionErrorCode != 0) {
+        messageFinish.innerHTML = `<p>Session ended abnormally. Reason: ${sessionInfo.sessionErrorMsg}</p>`;
+    } else {
+        messageFinish.innerHTML = `<p>You have completed the session.</p>`;
+    }
+    thisSession = sessionInfo;
+}
+
+// This callback function is triggered when this client gains control over dynamic objects
+export function gainedControl() {
+    myconsolelog('Client gained control');
+}
+
+// This  callback function is triggered when this client loses control over dynamic objects
+// e.g., when client's browser loses focus
+export function losesControl() {
+    myconsolelog('Client loses control');
+}
+
+// -------------------------------------------------------------------------------------
+//       Handle events triggered by MPLIB related to changes in the game state
+// -------------------------------------------------------------------------------------
+
+
+// -------------------------------------
+//       Display Information
+// -------------------------------------
+function myconsolelog(message) {
+    if (verbosity > 0) {
+        console.log(message);
+    }
+}
+
+
+// Converts the server-side timestamp expressed in milliseconds since the Unix Epoch to a string in local time
+function timeStr(timestamp) {
+    let date = new Date(timestamp);  // JavaScript uses milliseconds
+
+    // Add leading zero to hours, minutes, and seconds if they are less than 10
+    let hours = ("0" + date.getHours()).slice(-2);
+    let minutes = ("0" + date.getMinutes()).slice(-2);
+    let seconds = ("0" + date.getSeconds()).slice(-2);
+
+    let timeString = `${hours}:${minutes}:${seconds}`;
+    return timeString;
+}
+
+// Takes the URL parameters to update the session configuration
+function updateConfigFromUrl( sessionConfig ) {
+    const url = window.location.href;
+    const urlParams = new URL(url).searchParams;
+
+    for (let key in sessionConfig) {
+        if (urlParams.has(key)) {
+            const value = urlParams.get(key);
+
+            let newValue;
+            if (!isNaN(value)) {
+                newValue = Number(value);
+            }
+            else if (value === 'true' || value === 'false') {
+                newValue = (value === 'true');
+            }
+            // if not a number or boolean, treat it as a string
+            else {
+                newValue = value;              
+            }
+            sessionConfig[key] = newValue;
+            myconsolelog( `URL parameters update session parameter ${key} to value ${newValue}`);
+        }
+    }
+}
